08c5c83fc881c2b65350bffa6a79a58911b73fac
juanpabloramosp <pablo.ramos@munialajuela.go.cr>
2021-03-30 11:39:12 -0600

    Metodo1 Completo

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,11 @@ namespace ConsoleApp1
 {
     class Program
     {
+
+        public void Metodo1()
+        {
+            ///
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
